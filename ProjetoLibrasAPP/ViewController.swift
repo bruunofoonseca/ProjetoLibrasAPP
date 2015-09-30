bae43fa4f8a431410bc64f8cde18a85ca0b5cdea
Bruno Pereira <brunopereiras@icloud.com>
2015-09-30 19:04:56 -0300

    Initial Commit

---
+++ ProjetoLibrasAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetoLibrasAPP
+//
+//  Created by Bruno Pereira dos Santos on 30/09/15.
+//  Copyright © 2015 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ProjetoLibrasAPPTests/ProjetoLibrasAPPTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjetoLibrasAPPTests.swift
+//  ProjetoLibrasAPPTests
+//
+//  Created by Bruno Pereira dos Santos on 30/09/15.
+//  Copyright © 2015 BEPiD. All rights reserved.
+//
+
+import XCTest
+@testable import ProjetoLibrasAPP
+
+class ProjetoLibrasAPPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
