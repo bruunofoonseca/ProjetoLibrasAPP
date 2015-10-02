302c90c2f4a95e410ee29edddba788b5f3874e26
Gabriela Gomes <gah__@hotmail.com>
2015-10-02 00:08:02 -0300

    artigo no singular
    artigo no singular concordando com o gênero da frase. Letra maíuscula
    caso seja inicio da frase, ou minúscula caso não seja.

--- ProjetoLibrasAPP/ViewController.swift
+++ ProjetoLibrasAPP/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let metodo = Translator()
+    let artigo = Artigo()
+    var ab : [Word] = []
+    var ac : [String] = []
+    var novaFrase: [String] = []
+    var juntaPalavras : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Aonde colocamos a frase
+        let frase = "bolo bola"
+        
+        //chama os metodos de classificar e colocar artigos.
+        self.ab = metodo.test_classify(frase)
+        self.ac = artigo.colocaArtigoDefinido(ab)
+        
+        //feito para juntar as palavras em uma String e colocar na tela.
+        metodoJuntaPalavra()
+    }
+    
+    func metodoJuntaPalavra(){
+        for i in 0...self.ab.count - 1{
+            if ac[i] != ""{
+                juntaPalavras += ac[i] + " "
+            }
+            juntaPalavras += ab[i].text + " "
+        }
+        
+        print(juntaPalavras)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
