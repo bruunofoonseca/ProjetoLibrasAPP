4c730eca78e5709d2e52a6adb97588d3114e4637
Gabriela Gomes <gah__@hotmail.com>
2015-10-20 18:56:38 -0200

    preposição entre verbos fora da lista

--- ProjetoLibrasAPP/VerboEstrutura.swift
+++ ProjetoLibrasAPP/VerboEstrutura.swift
@@ -24,13 +24,22 @@ class VerboEstrutura : NSObject {
     {
         defVerbo.removeAll()
         
+        var posVerbo = -1
+        
+        for (var i = 0; i < frase[1].categories.count; i++){
+            
+            if (frase[1].categories[i].text == "verbo") && (frase[1].flexions[i].text == "Infinitivo Flexionado - 1ª singular"){
+                posVerbo = i
+            }
+        }
+        
         if (frase[1].categories[0].text == "verbo"){
             defVerbo.append(objVerbo.conjugVerboSujeito(frase[0], verbo: frase[1]))
         }
         else{
             defVerbo.append(frase[1].text)
         }
-        defVerbo.append(objPreposicao.inserePreposicao(frase))
+        defVerbo.append(objPreposicao.inserePreposicao(frase, posicao: posVerbo))
         
         return defVerbo
     }
